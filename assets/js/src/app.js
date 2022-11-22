564ef25ebe068342a53dd7e28049378be7c6f04d
ocdladefense <admin@ocdla.org>
2022-11-22 14:12:06 -0800

    Initial commit

---
+++ assets/js/src/app.js
@@ -0,0 +1,17 @@
+/** @jsx vNode */
+
+
+/**
+ * This is our list of components to be used in the app.
+**/
+import { vNode, View } from '/node_modules/@ocdladefense/view/view.js';
+
+
+
+function init() {
+  
+
+}
+
+
+domReady(init);
---
+++ assets/js/src/components.js
@@ -0,0 +1,24 @@
+/** @jsx vNode */
+
+
+/**
+ * This is our list of components to be used in the app.
+**/
+import { vNode, View } from '/node_modules/@ocdladefense/view/view.js';
+
+
+export { OcdlaInfoWindow };
+
+
+
+
+
+const OcdlaInfoWindow = function(props) {
+    let contact = props.member;
+
+    return (
+        <div>
+            {member.FirstName member.LastName}}
+        </div>
+    )
+};
